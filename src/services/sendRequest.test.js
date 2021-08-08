b4eb6e6fa5b088067365dbec9046ac932efcc4c3
culi <culitif@tuta.io>
2021-08-08 16:09:20 -0700

    fix sendRequest test

--- src/services/sendRequest.test.js
+++ src/services/sendRequest.test.js
@@ -1,9 +1,10 @@
 /* eslint-disable max-len */
-import sendRequest from '/sendRequest.js';
+import sendRequest from './sendRequest.js';
 
 const API_CALLS = [
   {
     url: 'https://www.onezoom.org/popularity/list?key=0&otts=563159&expand_taxa=True&max=12&names=True&include_raw=True&sort=standard',
+    params: {},
     request: {
       method: 'GET'
     }
@@ -25,16 +26,17 @@ const API_CALLS = [
   },
   {
     url: 'https://api.opentreeoflife.org/v3/tnrs/autocomplete_name',
+    params: {},
     request: {
       method: 'POST',
-      body: JSON.stringify({ name: 'Acer alba', context_name: 'All life' })
+      body: JSON.stringify({ name: 'Quercus alba', context_name: 'All life' })
     }
   }
 ];
 
 describe('test sendRequest calls', () => {
   test('test a GET call with params in url', async () => {
-    const resp = await sendRequest(API_CALLS[0]);
+    const resp = await sendRequest(...Object.values(API_CALLS[0]));
     const json = await resp.json();
 
     expect(resp.status).toBe(200);
@@ -42,7 +44,7 @@ describe('test sendRequest calls', () => {
   });
 
   test('test a POST call with a body', async () => {
-    const resp = await sendRequest(API_CALLS[2]);
+    const resp = await sendRequest(...Object.values(API_CALLS[2]));
     const json = await resp.json();
 
     expect(resp.status).toBe(200);
